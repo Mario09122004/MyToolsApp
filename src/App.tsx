9daccb4a779177f76716734266c096783656c47f
Mario Lira <mariorapido5560@hotmail.com>
2026-05-04 13:16:58 -0600

    Save area and gluestack-ui v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import "./../global.css"
+import "./../global.css";
+
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
+import { Box } from '@/components/ui/box';
+import { Fab, FabIcon, FabLabel } from '@/components/ui/fab';
+import { AddIcon } from '@/components/ui/icon';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaProvider>
+      <GluestackUIProvider mode="dark">
+        <SafeAreaView style={{ flex: 1 }}>
+          <Box className="h-full w-full bg-background-50 rounded-md">
+
+            <Text className="text-xl font-bold text-blue-500">
+              Welcome to Nativewind!
+            </Text>
+
+            <Fab
+              size="lg"
+              placement="bottom right"
+              isHovered={false}
+              isDisabled={false}
+              isPressed={false}
+            >
+              <FabIcon as={AddIcon} />
+              <FabLabel>Create</FabLabel>
+            </Fab>
+          </Box>
+        </SafeAreaView>
+      </GluestackUIProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
