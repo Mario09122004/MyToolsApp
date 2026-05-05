b6903e48891f00f713b63b082fdbd34bd410a81d
Mario Lira <mariorapido5560@hotmail.com>
2026-05-05 15:12:18 -0600

    feat: db save notes

--- src/layout/ScreenLayout.tsx
+++ src/layout/ScreenLayout.tsx
@@ -4,6 +4,7 @@ import {
 } from 'react-native-safe-area-context';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 //db
 import { Suspense } from 'react';
@@ -13,11 +14,32 @@ import { drizzle } from 'drizzle-orm/expo-sqlite';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import migrations from '@/drizzle/migrations';
 
-export const DATABASE_NAME = 'tasks';
-const queryClient = new QueryClient();
-
+export const DATABASE_NAME = 'tasks.db';
+const expoDb = openDatabaseSync(DATABASE_NAME);
+const db = drizzle(expoDb);
 
 export const ScreenLayout = ({ children }: { children: React.ReactNode }) => {
+    const { success, error } = useMigrations(db, migrations);
+
+
+    if (error) {
+        return (
+            <SafeAreaView className="flex-1 justify-center items-center">
+                <Text>Error de automatización: {error.message}</Text>
+            </SafeAreaView>
+        );
+    }
+
+    if (!success) {
+        return (
+            <SafeAreaView className="flex-1 justify-center items-center bg-black">
+                <ActivityIndicator size="large" color="#FF0055" />
+                <Text className="text-white mt-4 font-bold">
+                    Actualizando esquemas...
+                </Text>
+            </SafeAreaView>
+        );
+    }
 
     return (
         <Suspense fallback={<ActivityIndicator size="large" />}>
