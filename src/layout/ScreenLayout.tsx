37e0793d394cbf9dc8ebb7bc5dce85bbd246a251
Mario Lira <mariorapido5560@hotmail.com>
2026-05-05 19:24:02 -0600

    feat: init menu
    In the layout, I added a button and simple text

--- src/layout/ScreenLayout.tsx
+++ src/layout/ScreenLayout.tsx
@@ -4,21 +4,39 @@ import {
 } from 'react-native-safe-area-context';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 //db
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { SQLiteProvider, openDatabaseSync } from 'expo-sqlite';
 import { drizzle } from 'drizzle-orm/expo-sqlite';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import migrations from '@/drizzle/migrations';
 
+//Menu
+import {
+    Drawer,
+    DrawerBackdrop,
+    DrawerContent,
+    DrawerHeader,
+    DrawerBody,
+    DrawerFooter,
+    DrawerCloseButton,
+} from '@/components/ui/drawer';
+import { Button, ButtonText } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
+import { Icon, CloseIcon } from '@/components/ui/icon';
+import { Box } from '@/components/ui/box';
+import { Divider } from '@/components/ui/divider';
+
 export const DATABASE_NAME = 'tasks.db';
 const expoDb = openDatabaseSync(DATABASE_NAME);
 const db = drizzle(expoDb);
 
 export const ScreenLayout = ({ children }: { children: React.ReactNode }) => {
+    const [showDrawer, setShowDrawer] = useState(false);
     const { success, error } = useMigrations(db, migrations);
 
     if (error) {
@@ -49,6 +67,50 @@ export const ScreenLayout = ({ children }: { children: React.ReactNode }) => {
                 <SafeAreaProvider>
                     <GluestackUIProvider>
                         <SafeAreaView style={{ flex: 1 }}>
+                            <Box>
+                                <Button
+                                    onPress={() => {
+                                        setShowDrawer(true);
+                                    }}
+                                >
+                                    <ButtonText>Open Drawer</ButtonText>
+                                </Button>
+                                <Heading>
+                                    Title of the component
+                                </Heading>
+                                <Drawer
+                                    isOpen={showDrawer}
+                                    size="full"
+                                    anchor="top"
+                                    onClose={() => {
+                                        setShowDrawer(false);
+                                    }}
+                                >
+                                    <DrawerBackdrop />
+                                    <DrawerContent>
+                                        <DrawerHeader>
+                                            <Heading size="lg">Menu</Heading>
+                                            <DrawerCloseButton>
+                                                <Icon as={CloseIcon} />
+                                            </DrawerCloseButton>
+                                        </DrawerHeader>
+                                        <DrawerBody>
+                                            <Text>This is the basic drawer component.</Text>
+                                        </DrawerBody>
+                                        <DrawerFooter>
+                                            <Button
+                                                variant="outline"
+                                                onPress={() => {
+                                                    setShowDrawer(false);
+                                                }}
+                                            >
+                                                <ButtonText>Cancel</ButtonText>
+                                            </Button>
+                                        </DrawerFooter>
+                                    </DrawerContent>
+                                </Drawer>
+                            </Box>
+                            <Divider className="my-0.5" />
                             {children}
                         </SafeAreaView>
                     </GluestackUIProvider>
