d68651ac903429478c9dd9660f55df01f1b669ba
Mario Lira <mariorapido5560@hotmail.com>
2026-05-13 11:57:24 -0600

    Feat: divition in roulette

--- src/components/Random/Roulette.tsx
+++ src/components/Random/Roulette.tsx
@@ -17,13 +17,18 @@ export const Roulette = ({ options }: { options: String[] }) => {
         )
     }
 
+    console.log("opciones:", options)
+
     return (
         <View className="bg-blue-500 items-center">
             <Svg height="50%" width="100%" viewBox="0 0 100 100">
                 <Circle cx="50" cy="50" r="45" stroke="red" strokeWidth="2.5" fill="green" />
                 {
                     option.map((data, index) => (
-                        <Line x1="50" y1="50" x2="50" y2="5" stroke="red" strokeWidth="2.5" transform={`rotate(${index * rotation}, 50, 50)`} key={index} />
+                        <>
+
+                            <Line x1="50" y1="50" x2="50" y2="5" stroke="red" strokeWidth="2.5" transform={`rotate(${index * rotation}, 50, 50)`} key={index} />
+                        </>
                     ))
                 }
                 <Line x1="50" y1="50" x2="50" y2="5" stroke="black" strokeWidth="2.5" transform="rotate(0, 50, 50)" />
@@ -31,3 +36,4 @@ export const Roulette = ({ options }: { options: String[] }) => {
         </View>
     )
 }
+
