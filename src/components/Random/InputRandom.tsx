d9ccd0b3e2d69cf4e8074d8fe31b129eb9a2be01
Mario Lira <mariorapido5560@hotmail.com>
2026-05-13 10:34:18 -0600

    Feat: Text area in Random

---
+++ src/components/Random/InputRandom.tsx
@@ -0,0 +1,40 @@
+import { View } from "react-native"
+import { Textarea, TextareaInput } from '@/components/ui/textarea';
+import {
+    FormControl,
+    FormControlError,
+    FormControlLabel,
+    FormControlLabelText,
+    FormControlHelper,
+    FormControlHelperText,
+} from '@/components/ui/form-control';
+import { useState } from "react";
+
+export const InputRandom = () => {
+    const [options, setOptions] = useState("")
+
+    const textInput = (text: string) => {
+        setOptions(text);
+        console.log(options);
+    }
+
+    const handleSeparateOptions = (text: string) => {
+        console.log("Texto separado: ", text.split("\n"));
+    }
+
+    return (
+        <View className="w-full p-2">
+            <FormControl size="md" className="w-full p-2">
+                <FormControlLabel>
+                    <FormControlLabelText>Write the options.</FormControlLabelText>
+                </FormControlLabel>
+                <Textarea>
+                    <TextareaInput placeholder="Option 1, \n Option 2, \n Option 3 ..." className="w-full" value={options} onChangeText={(text) => textInput(text)} />
+                </Textarea>
+                <FormControlHelper>
+                    <FormControlHelperText>Separate each option by enter.</FormControlHelperText>
+                </FormControlHelper>
+            </FormControl>
+        </View>
+    )
+}
