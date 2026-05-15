807f93bdf8ffe5bf4b0a6bf5cc59b26fbb11ca2b
Mario Lira <mariorapido5560@hotmail.com>
2026-05-14 19:25:02 -0600

    feat: lines separation in rulette

--- src/components/Random/Roulette.tsx
+++ src/components/Random/Roulette.tsx
@@ -1,13 +1,23 @@
-import { Text } from "@/components/ui/text";
 import { useEffect, useState } from "react";
 import { View } from "react-native";
-import Svg, { Circle, Rect, Line } from 'react-native-svg';
+import Svg, { Circle, Rect, Line, Text } from 'react-native-svg';
 
 const OptionsRoulette = { option: String }
 
 export const Roulette = ({ options }: { options: String[] }) => {
     const [option, setOption] = useState<String[]>(options);
-    const [rotation, setRotation] = useState<number>(option.length / 360);
+    const [rotation, setRotation] = useState<number>(0);
+
+    console.log("opciones entraas:", option)
+    console.log("Rotacion: ", rotation)
+    useEffect(() => {
+        console.log("Recreqando...")
+        setOption(options);
+        console.log("opciones:", option)
+        console.log(option.length)
+        setRotation(360 / option.length)
+        console.log("Rotacion: ", rotation)
+    }, [option, rotation, options])
 
     if (options.length < 2) {
         return (
@@ -17,8 +27,6 @@ export const Roulette = ({ options }: { options: String[] }) => {
         )
     }
 
-    console.log("opciones:", options)
-
     return (
         <View className="bg-blue-500 items-center">
             <Svg height="50%" width="100%" viewBox="0 0 100 100">
@@ -26,12 +34,11 @@ export const Roulette = ({ options }: { options: String[] }) => {
                 {
                     option.map((data, index) => (
                         <>
-
+                            <Text transform={`rotate(${index * rotation}, 50, 50)`}>{data}</Text>
                             <Line x1="50" y1="50" x2="50" y2="5" stroke="red" strokeWidth="2.5" transform={`rotate(${index * rotation}, 50, 50)`} key={index} />
                         </>
                     ))
                 }
-                <Line x1="50" y1="50" x2="50" y2="5" stroke="black" strokeWidth="2.5" transform="rotate(0, 50, 50)" />
             </Svg>
         </View>
     )
