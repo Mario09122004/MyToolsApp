9963b058345d3845ac3e3f706c15b0dd8b2067f0
Mario Lira <mario.alberto.lira.zamora@gmail.com>
2026-06-17 11:42:43 -0600

    db (habit): generate db

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -7,6 +7,7 @@ import m0002 from './0002_daffy_crusher_hogan.sql';
 import m0003 from './0003_material_deathbird.sql';
 import m0004 from './0004_ancient_lenny_balinger.sql';
 import m0005 from './0005_keen_blue_blade.sql';
+import m0006 from './0006_woozy_agent_brand.sql';
 
   export default {
     journal,
@@ -16,7 +17,8 @@ m0001,
 m0002,
 m0003,
 m0004,
-m0005
+m0005,
+m0006
     }
   }
   
