8d200d12ece2ca9a7550815a6b7095a1bfade981
Mario Lira <mario.alberto.lira.zamora@gmail.com>
2026-06-18 18:45:57 -0600

    fix: change ScrollView import from react-native-gesture-handler to react-native

--- src/components/Habits/habits.tsx
+++ src/components/Habits/habits.tsx
@@ -1,4 +1,4 @@
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView } from "react-native";
 import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
 import { Icon, InfoIcon } from "@/components/ui/icon";
