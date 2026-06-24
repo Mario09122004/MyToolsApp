a4eaa7e8c14137fafae55a0709dd11cd97a55340
Mario Lira <mario.alberto.lira.zamora@gmail.com>
2026-06-24 09:39:31 -0600

    fix(db): fix foreign key and column mapping in migration 0009

--- drizzle/0009_dark_human_robot.sql
+++ drizzle/0009_dark_human_robot.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys=OFF;
+--> statement-breakpoint
 CREATE TABLE `project` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
@@ -5,8 +7,8 @@ CREATE TABLE `project` (
 	`dueday` integer NOT NULL
 );
 --> statement-breakpoint
-DROP TABLE `proyect`;--> statement-breakpoint
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
+DROP TABLE IF EXISTS `proyect`;
+--> statement-breakpoint
 CREATE TABLE `__new_phases` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`projectId` integer NOT NULL,
@@ -18,9 +20,15 @@ CREATE TABLE `__new_phases` (
 	FOREIGN KEY (`projectId`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-INSERT INTO `__new_phases`("id", "projectId", "name", "description", "completed", "dueday", "order") SELECT "id", "projectId", "name", "description", "completed", "dueday", "order" FROM `phases`;--> statement-breakpoint
-DROP TABLE `phases`;--> statement-breakpoint
-ALTER TABLE `__new_phases` RENAME TO `phases`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;--> statement-breakpoint
-ALTER TABLE `taskPerPhase` ADD `dueday` integer NOT NULL;--> statement-breakpoint
-ALTER TABLE `taskPerPhase` ADD `order` integer NOT NULL;
+INSERT INTO `__new_phases`("id", "projectId", "name", "description", "completed", "dueday", "order") 
+SELECT "id", "proyectId", "name", "description", "completed", 0, 0 FROM `phases`;
+--> statement-breakpoint
+DROP TABLE IF EXISTS `phases`;
+--> statement-breakpoint
+ALTER TABLE `__new_phases` RENAME TO `phases`;
+--> statement-breakpoint
+PRAGMA foreign_keys=ON;
+--> statement-breakpoint
+ALTER TABLE `taskPerPhase` ADD `dueday` integer NOT NULL DEFAULT 0;
+--> statement-breakpoint
+ALTER TABLE `taskPerPhase` ADD `order` integer NOT NULL DEFAULT 0;
